84971814da791abd606eb185f8f75763ad8e570d
Arya Laddha <z5340668@unsw.edu.au>
2023-01-24 16:49:57 +1100

    added navbar rough idea

---
+++ personal-website/src/components/navbar/MenuItems.js
@@ -0,0 +1,35 @@
+export const MenuItems = [
+    {
+        url: "",
+        cName: "nav-links",
+        icon: "fa fa-user"
+    },
+    {
+        url: "",
+        cName: "nav-links",
+        icon: "fa-solid fa-heart"
+    },
+    {
+        url: "",
+        cName: "nav-links",
+        icon: "fa-solid fa-cart-shopping"
+    }
+]
+
+export const MenuItems2 = [
+    {
+        url: "",
+        cName: "nav-links-2",
+        title: "Women"
+    },
+    {
+        url: "",
+        cName: "nav-links-2",
+        title: "Men"
+    },
+    {
+        url: "",
+        cName: "nav-links-2",
+        title: "Kids"
+    },
+]
