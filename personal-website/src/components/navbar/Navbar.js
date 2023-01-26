1725fa3be2d2fe8f62dce4d89b54f17db421cef7
Arya Laddha <z5340668@unsw.edu.au>
2023-01-26 14:09:33 +1100

    navbar changes and media screen queries to make it responsive

--- personal-website/src/components/navbar/Navbar.js
+++ personal-website/src/components/navbar/Navbar.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import "./Navbar.css"
+import logo from '../../images/newLogo1.png'
+import instagramLogo from '../../images/icons8-instagram-48.svg'
+import githubLogo from '../../images/icons8-github-60.svg'
+import linkedinLogo from '../../images/icons8-linkedin.svg'
+import facebookLogo from '../../images/icons8-facebook.svg'
 
 class Navbar extends React.Component {
     render() {
@@ -7,16 +12,28 @@ class Navbar extends React.Component {
             <nav>
                 <div class="navbar-content">
                     <div class="left-content">
-                        Arya Laddha
+                        <img src={logo} alt="logo" className="logoMain" height={50} width={50}/>
                     </div>
                     <div class="center-content">
                         <a href="#home">Home</a>
                         <a href="#about">About</a>
-                        <a href="#skills">Skills</a>
-                        <a href="#work">Work</a>
+                        <a href="#skills">Portfolio</a>
+                        <a href="#work">Blog</a>
+                        <a href="#work">Contact</a>
                     </div>
                     <div class="right-content">
-                        <button>Contact</button>
+                        <a href='#' className="social-link">
+                            <img src={instagramLogo} alt="logo" className="social-logo" />
+                        </a>
+                        <a href='#' className="social-link">
+                            <img src={githubLogo} alt="logo" className="social-logo" height={48} width={48}/>
+                        </a>
+                        <a href='#' className="social-link">
+                            <img src={linkedinLogo} alt="logo" className="social-logo" />
+                        </a>
+                        <a href='#' className="social-link">
+                            <img src={facebookLogo} alt="logo" className="social-logo" /> 
+                        </a>
                     </div>
                 </div>
             </nav>
