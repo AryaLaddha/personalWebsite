c6c7904c852f6edc2cd5b307c6a9bf7ec4ce0c9f
Arya Laddha <z5340668@unsw.edu.au>
2023-01-27 13:56:57 +1100

    fixed background image

--- personal-website/src/components/backgroundHome/BackgroundHome.js
+++ personal-website/src/components/backgroundHome/BackgroundHome.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import "./BackgroundHome.css"
-import logo from '../../images/backgroundImage.jpg'
+import logo from '../../images/background1.png'
 
 class BackgroundPage extends React.Component {
     render () {
         return (
             <div className="background-main">
-                <div className="backgroundPage-left">
-                    
-                </div>
                 <div className="backgroundPage">
                     <img src={logo} alt="logo" className="background" />
-                </div>
-                <div className="backgroundPage-right">
                 </div>
             </div>
         )
