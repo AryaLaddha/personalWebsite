89b4a8d51185acdc122a36c07968efa0a7e35b57
Arya Laddha <z5340668@unsw.edu.au>
2023-01-27 23:45:15 +1100

    added about files, fixed background and programming icons

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/navbar/Navbar'
 import Backgroundhome from './components/backgroundHome/BackgroundHome'
 import Programming from './components/programmingLanguages/Programming'
+import About from './components/about/About'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Backgroundhome />
       <Programming />
+      <About />
     </div>
   );
 }
---
+++ personal-website/src/components/about/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './About.css'
+
+class About extends React.Component {
+    render () {
+        return (
+            <div className="AboutPage">
+            </div>
+        )
+    }
+}
+
+export default About;
