b274002fc138e5a4b0451797381d9fda5d18a4b8
Arya Laddha <z5340668@unsw.edu.au>
2023-01-27 16:23:00 +1100

    completed background

--- personal-website/src/components/backgroundHome/BackgroundHome.js
+++ personal-website/src/components/backgroundHome/BackgroundHome.js
@@ -5,11 +5,27 @@ import logo from '../../images/background1.png'
 class BackgroundPage extends React.Component {
     render () {
         return (
-            <div className="background-main">
-                <div className="backgroundPage">
-                    <img src={logo} alt="logo" className="background" />
+                <div className="background-main">
+                    <div className="backgroundPage-left">
+                        <div className="backgroundPage-left-up">
+                            Hi, I am Arya.
+                        </div>
+                        <div className="backgroundPage-left-down">
+                            <div className="backgroundPage-left-down-up">
+                                Web Developer
+                            </div>
+                            <div className="backgroundPage-left-down-down">
+                                & Data Analyst
+                            </div>
+                            <div className="backgroundPage-left-lowest">
+                                I am a Computer Science student at the University of New South Wales and working part-time as a systems engineer. Aspiring to be an amazing frontend developer and data analyst to help companies grow their business.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="backgroundPage">
+                        <img src={logo} alt="logo" className="background" />
+                    </div>
                 </div>
-            </div>
         )
     }
 }
