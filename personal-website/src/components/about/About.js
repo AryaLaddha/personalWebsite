e447cae5b6b0d89fe9a11e789278c640fe8d7358
Arya Laddha <z5340668@unsw.edu.au>
2023-01-28 17:55:47 +1100

    changed about file needs more work

--- personal-website/src/components/about/About.js
+++ personal-website/src/components/about/About.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './About.css'
+import Profile from '../../images/IMG_5391.jpg'
 
 class About extends React.Component {
     render () {
         return (
             <div className="AboutPage">
-                <div class="container">
-                    <div class="line left"></div>
-                    <div class="text">ABOUT</div>
-                    <div class="line right"></div>
+                <div classname="about-container">
+                    <div classname="about-line-left"></div>
+                    <div classname="About-text">ABOUT</div>
+                    <div classname="about-line-right"></div>
+                </div>
+                <div classname="main-about">
+                    <div classname="paragraph-about"></div>
+                    <div classname="picture"></div>
                 </div>
             </div>
         )
