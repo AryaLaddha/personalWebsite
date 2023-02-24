d66f945f4616587a12847116e7bf53053ab5ce13
Arya Laddha <z5340668@unsw.edu.au>
2023-02-24 15:12:53 +1100

    added basic template for work section

--- personal-website/src/components/parentComponent/ParentComponent.js
+++ personal-website/src/components/parentComponent/ParentComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BackgroundHome from '../backgroundHome/BackgroundHome';
 import About from '../about/About';
+import Work from '../work/Work';
 
 const ParentComponent = () => {
     return (
         <div className="parentComponent">
             <BackgroundHome />
             <About />
+            <Work />
         </div>
     );
 };
---
+++ personal-website/src/components/work/Work.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Work.css'
+
+class Work extends React.Component {
+    render () {
+        return (
+            <div className="Work-page">
+                Hello
+            </div>
+        )
+    }
+}
+
+export default Work;
