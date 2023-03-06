bf55a299f62eb7d8208578c24b6ea55efbf315b4
Arya Laddha <z5340668@unsw.edu.au>
2023-03-06 17:41:30 +1100

    Added work back, making a timeline structure

--- personal-website/src/components/parentComponent/ParentComponent.js
+++ personal-website/src/components/parentComponent/ParentComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BackgroundHome from '../backgroundHome/BackgroundHome';
 import About from '../about/About';
+import Work from '../work/Work'
 import Projects from '../projects/Projects';
 
 const ParentComponent = () => {
@@ -8,6 +9,7 @@ const ParentComponent = () => {
         <div className="parentComponent">
             <BackgroundHome />
             <About />
+            <Work />
             <Projects />
         </div>
     );
